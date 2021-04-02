2807213b82f3f176431cee419a585154227a6be4
imkpk <imkpk@live.com>
2021-04-02 23:49:47 +0530

    added client

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-// const path = require('path');
+const http = require('http');
+const path = require('path');
 const express = require('express');
 const app = express();
+const socketio = require('socket.io');
 require('dotenv').config();
 
-const socketIo = require('socket.io');
-
-const http = require('http');
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketio(server);
 // env variables
 const PORT = process.env.PORT;
 const public_path = process.env.PATH;
 
-// set static folder
-app.use(express.static(public_path));
-// socket io
-io.on('connection', (socket) => {
-  console.log('new connection');
-});
 // app.listen
 server.listen(PORT, () => {
   console.log(`server is runnign on prot number ${PORT}`);
+});
+
+// set static folder path
+app.use(express.static(public_path));
+// socket io
+io.on('connection', (socket) => {
+  console.log('new ws connection');
+  socket.emmit('message', 'welcome chat course');
+
+  // broadcast when user connecgs
+  socket.broadcast.emit();
 });
